47ad3d3dc1e824d79e71945f95e4bfff4c8b7db4
Leandrobftt <149838874+Leandrobftt@users.noreply.github.com>
2024-10-13 19:58:29 -0300

    feat/login

---
+++ projeto-adotapet/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace projeto_adotapet.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+    }
+}
--- projeto-adotapet/Program.cs
+++ projeto-adotapet/Program.cs
@@ -1,9 +1,14 @@
+using projeto_adotapet.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
